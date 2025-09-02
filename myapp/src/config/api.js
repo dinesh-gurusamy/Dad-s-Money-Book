462091c1a77037f70867ff724bd27f15167c4993
Dinesh <dineshgurusamy18@gmail.com>
2025-09-02 21:02:08 +0530

    Updated project code

--- myapp/src/config/api.js
+++ myapp/src/config/api.js
@@ -5,6 +5,7 @@ const API = axios.create({
   baseURL: import.meta.env.REACT_APP_API_URL || "http://localhost:5000/api",
 });
 
+console.log(import.meta.env.REACT_APP_API_URL);
 // Auto-attach token
 API.interceptors.request.use((config) => {
   const userInfo = JSON.parse(localStorage.getItem("userInfo"));
